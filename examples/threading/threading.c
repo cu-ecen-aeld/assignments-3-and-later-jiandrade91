4f8400de88979b8383b2c3a290c98eab717ad34d
jiandrade91 <jiandrade91@gmail.com>
2024-03-02 15:17:35 -0800

    threading 1st attempt

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -4,8 +4,11 @@
 #include <stdio.h>
 
 // Optional: use these functions to add debug or error prints to your application
-#define DEBUG_LOG(msg,...)
+//#define DEBUG_LOG(msg,)
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
+//#define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
+
+#define DEBUG_LOG(msg,...) printf(msg, ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
 
 void* threadfunc(void* thread_param)
@@ -14,6 +17,29 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    thread_func_args->thread_complete_success = false;
+    
+    usleep(1000*(thread_func_args->wait_to_obtain_ms));
+    DEBUG_LOG("****Waiting to OBTAIN Mutex****\n");
+
+    if(pthread_mutex_lock(thread_func_args->mutex) != 0){
+	    ERROR_LOG("****Mutex Not Locked****\n");
+	    DEBUG_LOG("****Mutex Not Locked****\n");
+	    return thread_func_args;
+
+    }
+
+    usleep(1000*(thread_func_args->wait_to_release_ms));
+    DEBUG_LOG("****Waiting to RELEASE Mutex****\n");
+
+    if (pthread_mutex_unlock(thread_func_args->mutex) != 0) {
+        DEBUG_LOG("****Mutex Locked****\n");
+        ERROR_LOG("****Mutex Locked****\n");
+        return thread_func_args;
+    }
+    thread_func_args->thread_complete_success = true;
+    
     return thread_param;
 }
 
@@ -28,6 +54,27 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data *local_data;
+    thread_data_local = malloc(sizeof(struct thread_data));
+
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setstacksize(&attr, 2400);
+
+    local_data->thread = thread;
+    local_data->mutex = mutex;
+    local_data->wait_to_obtain_ms = wait_to_obtain_ms;
+    local_data->wait_to_release_ms = wait_to_release_ms;
+
+    int thread_status = pthread_create(thread, &attr, threadfunc, (void*)local_data);
+
+    if (thread_status) {
+        DEBUG_LOG("****Thread not created****\n");
+        return false;
+    }
+    else {
+        DEBUG_LOG("****Thread created successfully!****\n");
+        return true;
+    }
 }
 
